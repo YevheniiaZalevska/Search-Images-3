e5ba24542bb9ae1b13b9b8c53d51ca3feed58a43
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-20 15:06:12 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,16 +1,26 @@
-import ImageCard from "../ImageCard/ImageCard";
-import s from "./ImageGallery.module.css";
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import ImageCard from '../ImageCard/ImageCard';
+import s from './ImageGallery.module.css';
 
-const ImageGallery = ({ articles, openModal }) => {
+const ImageGallery = forwardRef(({ images, onImageClick }, ref) => {
+  console.log('images: ', images);
   return (
-    <ul className={s.gallery}>
-      {articles.map((article) => (
-        <li key={article.id} className={s.galleryItem}>
-          <ImageCard article={article} openModal={openModal} />
+    <ul className={s.gallery} ref={ref}>
+      {images.map(image => (
+        <li key={image.id} className={s.galleryItem} >
+          <ImageCard info={image} onClick={() => onImageClick(image)}/>
         </li>
       ))}
     </ul>
   );
+});
+
+ImageGallery.displayName = 'ImageGallery';
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object),
+  onImageClick: PropTypes.func.isRequired,
 };
 
 export default ImageGallery;
