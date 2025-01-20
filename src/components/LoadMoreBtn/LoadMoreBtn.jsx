34a93c9570d0f99944d5127db6560a6ad33f9947
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-20 15:06:47 +0100

    update load more btn

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,13 +1,16 @@
-import s from "./LoadMoreBtn.module.css";
+import PropTypes from 'prop-types';
+import s from './LoadMoreBtn.module.css';
 
-const LoadMoreBtn = ({ onLoadMore }) => {
+const LoadMoreBtn = ({ onClick }) => {
   return (
-    <div className={s.wrapperLoadBtn}>
-      <button className={s.loadBtn} onClick={onLoadMore}>
-        Load more
-      </button>
-    </div>
+    <button className={s.loadMore} onClick={onClick} type="button">
+      Load More
+    </button>
   );
+};
+
+LoadMoreBtn.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default LoadMoreBtn;
